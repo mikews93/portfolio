85e864f1a7bb8334fb0cf9ed96ad1e579606a81a
Miguel Blanco <mblanco@indigoag.com>
2022-06-10 20:49:25 -0500

    fix: error with build

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App/App';
+import { App } from './components/App/App';
 import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
